124ee7d21cab71b00d0cf503896f2e27309122d6
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2018-03-28 14:57:15 -0600

    [DEPLOY] to heroku 4

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse
 
 
@@ -6,3 +8,7 @@ class HelloWorld(APIView):
 
     def get(self, request):
         return HttpResponse('Hello World Travis setup!')
+
+    def post(self, request):
+        return Response(request.data, status=status.HTTP_200_OK)
+        
