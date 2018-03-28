bea77446c2ff4f327a2baad938279b24f1b17df8
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2018-03-28 12:52:38 -0600

    [ADD] simple test

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class FirstTest(TestCase):
+
+    def setUp(self):
+        self.a = 1
+        self.b = 2
+
+    def test_suma(self):
+        self.assertEqual(self.a + self.b, 3)
